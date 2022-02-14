3b347b3c027540548184b7b414e44f282527caa6
KoJaem <woalswhwh@naver.com>
2022-02-14 23:06:23 +0900

    디지털칠판 디자인 및 데이터 입력 후 엔터입력 시 칠판에 출력되게 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "@emotion/styled";
+import { Blackboard } from "./page/Blackboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Blackboard />
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/page/Blackboard.jsx
@@ -0,0 +1,69 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+export const Blackboard = () => {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const onChange = e => {
+    setInput(e.target.value);
+  };
+
+  const onEnter = e => {
+    if (e.key === "Enter") {
+      setList(list.concat(input));
+      setInput("");
+    }
+  };
+  
+
+  return (
+    <Wrapper>
+      <ContentsArea>
+        {list.map((data, index) => (
+          <Contents id={index} key={index} >{data}</Contents>
+        ))}
+      </ContentsArea>
+      <Input
+        placeholder="입력해주세요"
+        onChange={onChange}
+        onKeyPress={onEnter}
+        value={input}
+      ></Input>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin-top: 20vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const ContentsArea = styled.div`
+  width: 50vw;
+  height: 30vh;
+  background-color: #2a7539;
+  box-sizing: border-box;
+  border: solid 1px;
+  border-bottom: 0;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    opacity: 0;
+  }
+`;
+const Contents = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 50vw;
+  height: 3vh;
+  text-align: center;
+  box-sizing: border-box;
+  border-top: 0;
+  border: solid 1px;
+`;
