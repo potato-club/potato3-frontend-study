fd9ec49026017a80c48717a2b2b283388074556e
SongJae Lim <thdwo999@naver.com>
2022-02-17 11:45:17 +0900

    기본 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>ต๐ม๖ละ ฤฅฦว</p>
+      <div className="board"></div>
     </div>
   );
 }
