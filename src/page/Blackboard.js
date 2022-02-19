c38adb1c4c286332a46dbc09fc3026ab553dae04
(hyungkey) <(hyungkey0601@gmail.com)>
2022-02-20 00:52:04 +0900

    줄바꿈 시도_실패

--- src/page/Blackboard.js
+++ src/page/Blackboard.js
@@ -1,17 +1,35 @@
 import { useState } from "react";
 
 export default function BlackBoard() {
-  const [NaeYong, CNaeYong] = useState("");
+  const [NaeYong, CNaeYong] = useState([""]);
+  const [Test, SetTest] = useState([""]);
 
   const PushText = (e) => {
     CNaeYong(e.target.value);
   };
 
+  function EnterKey(e) {
+    if (e.key == "Enter") {
+      SetTest(NaeYong + "\n");
+
+      CNaeYong("");
+    }
+  }
+
   return (
     <div className="App">
       <h1>디지털 칠판</h1>
-      <div className="MainText">{NaeYong}</div>
-      <input value={NaeYong} placeholder="입력해주세요" onChange={PushText} />
+      <div className="MainText">
+        {Test}
+
+        {NaeYong}
+      </div>
+      <input
+        value={NaeYong}
+        placeholder="입력해주세요"
+        onChange={PushText}
+        onKeyPress={EnterKey}
+      />
     </div>
   );
 }
